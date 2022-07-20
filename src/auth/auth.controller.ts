da4c9e1d062351eb4d6c5168ef2ac4b97f0b368d
areum <reumaco99@gmail.com>
2022-07-20 14:51:34 +0900

    Feature: 회원가입 기능 추가
    signUp 기능 추가
    POST `/sns/sign-up`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SignUpDto } from './dto/sign-up.dto';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('/sns/auth')
+export class AuthController {
+  constructor(private readonly service: AuthService) {}
+
+  @Post('/sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.service.signUp(signUpDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/User';
+import { Repository } from 'typeorm';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  signUp(signUpDto: SignUpDto) {
+    return this.repository.save(signUpDto);
+  }
+}
