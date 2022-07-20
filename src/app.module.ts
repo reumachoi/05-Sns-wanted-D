11d11cd5f62ef158c467dbd5239a0a41ef94a384
areum <reumaco99@gmail.com>
2022-07-20 11:43:33 +0900

    Setting: Post 모듈 생성
    게시글 작성, 수정, 삭제, 조회 등 담당 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 import * as ormConfig from '../ormConfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), AuthModule],
+  imports: [TypeOrmModule.forRoot(ormConfig), AuthModule, PostModule],
   controllers: [],
   providers: [],
 })
