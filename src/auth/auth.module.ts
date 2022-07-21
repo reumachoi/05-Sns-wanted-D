19c04440c616dbd72df53a3de364f95b4828e7c5
areum <reumaco99@gmail.com>
2022-07-21 12:50:42 +0900

    Fix: .env 파일 관련 토큰 가져오기 에러 해결
     JwtModule.register -> JwtModule.registerAsync
    ConfigModule, ConfigService로 .env파일 읽어오고난 후에 secret등록하기

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,15 +6,20 @@ import { User } from 'src/entities/User';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_ACCESS_TOKEN_SECRET,
-      signOptions: {
-        expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
-      },
+    JwtModule.registerAsync({
+      //.env 파일 읽어올때까지 secret등록 작업 유예
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+        signOptions: {
+          expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+        },
+      }),
     }),
     TypeOrmModule.forFeature([User]),
   ],
