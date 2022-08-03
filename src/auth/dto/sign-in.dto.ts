5208cbc38ef45a468f574f4634cd6d0bff9cf43d
areum <reumaco99@gmail.com>
2022-08-03 16:28:03 +0900

    Feature: 유저 회원가입, 로그인 DTO 유효성체크
    회원가입, 로그인시 유효성체크를 위한 코드 추가

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SignInDto {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   pwd: string;
 }
--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, Matches, Min } from 'class-validator';
+
 export class SignUpDto {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Matches(/^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{8,16}$/, {
+    message:
+      '비밀번호는 최소 하나의 문자, 숫자, 특수문자로 최소8자리에서 최대 16자리 입니다.',
+  })
   pwd: string;
+
+  @IsNotEmpty()
   nickname: string;
 }
